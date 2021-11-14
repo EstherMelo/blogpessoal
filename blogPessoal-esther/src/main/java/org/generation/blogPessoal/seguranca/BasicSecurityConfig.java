12f438fae395702bcc1f8a8880007dc1d719e993
EstherMelo <emelo3172@gmail.com>
2021-11-14 20:37:17 -0300

    atualização

--- blogPessoal-esther/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ blogPessoal-esther/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -35,9 +35,16 @@ public class BasicSecurityConfig  extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/usuarios/cadastrar").permitAll().antMatchers("/usuarios/logar")
-                .permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-                .csrf().disable();
+        http.authorizeRequests()
+        .antMatchers("/**").permitAll()
+        .antMatchers("/usuarios/cadastrar").permitAll()
+        .antMatchers("/usuarios/logar").permitAll()
+        .antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.GET ,"/tema").permitAll()
+        .antMatchers(HttpMethod.OPTIONS).permitAll()
+        .anyRequest().authenticated().and().httpBasic()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().cors().and()
+        .csrf().disable();
     }
 }
